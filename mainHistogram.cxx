b5c2cfb15c153919744aca13d15dce3f4f4e3e04
steven christe <steven.d.christe@nasa.gov>
2011-12-15 11:23:58 -0500

    added error bars to histogram plot

--- mainHistogram.cxx
+++ mainHistogram.cxx
@@ -1,8 +1,7 @@
 #include "mainHistogram.h"
 #include "gui.h"
+#include <math.h>
 
-#define XBORDER 0
-#define YBORDER 0
 #define XSTRIPS 128
 #define YSTRIPS 128
 #define TICKLENGTH 3
@@ -44,7 +43,7 @@ mainHistogram::mainHistogram(int x,int y,int w,int h,const char *l)
 		displayHistogram[i] = HistogramFunction[i];
 	}
 	FLHistcursorX[0] = 500;
-	mouseHistPixel = FLHistcursorX[0] - XBORDER;
+	mouseHistPixel = FLHistcursorX[0];
 }
 
 void mainHistogram::draw()
@@ -72,7 +71,7 @@ void mainHistogram::draw()
 	glLoadIdentity();
 	
 	glViewport(0,0,w(),h());
-	gluOrtho2D(0,MAX_CHANNEL/mainHistogram_binsize, 0, ymax+2*XBORDER);
+	gluOrtho2D(0,MAX_CHANNEL/mainHistogram_binsize, 0, ymax);
 	
 	glMatrixMode(GL_MODELVIEW);
 	glDisable(GL_DEPTH_TEST);
@@ -101,9 +100,11 @@ void mainHistogram::draw()
 	
 	for(int i = 0; i < MAX_CHANNEL/mainHistogram_binsize; i++)
 	{
-		glVertex2f(i+XBORDER, YBORDER + displayHistogram[i]);
-		glVertex2f((i+1)+XBORDER, YBORDER + displayHistogram[i]);
-		k++;
+		glVertex2f(i, displayHistogram[i]);
+		glVertex2f(i+1, displayHistogram[i]);
+		
+		glVertex2f(i+0.5, displayHistogram[i] - sqrt(displayHistogram[i]));
+		glVertex2f(i+0.5, displayHistogram[i] + sqrt(displayHistogram[i]));
 	
 	}
 	//glVertex2f(MAX_CHANNEL+XBORDER, YBORDER);
@@ -113,17 +114,17 @@ void mainHistogram::draw()
 	// draw vertival select line from mouse click
 	glBegin(GL_LINES);
 	glColor3f(0.0, 1.0, 0.0);
-	glVertex2f(FLHistcursorX[0], YBORDER);
+	glVertex2f(FLHistcursorX[0], 0);
 	glVertex2f(FLHistcursorX[0], ymax);
 	glEnd();
 	
 	// draw the line showing the low energy cutoff
 	glColor4f(0.0, 0.0, 1.0, 0.5);
-	glRectf(XBORDER, 0, low_threshold, ymax);
+	glRectf(0, 0, low_threshold, ymax);
 	
 	glBegin(GL_LINES);
 	glColor3f(0.0, 0.0, 1.0);
-	glVertex2f(low_threshold, YBORDER);
+	glVertex2f(low_threshold, 0);
 	glVertex2f(low_threshold, ymax);
 	glEnd();
 	
@@ -162,10 +163,10 @@ int mainHistogram::handle(int eventType)
 	if(eventType == FL_PUSH)
 	{
 		//convert between fltk coordinates and opengl coordinates
-		FLHistcursorX[0] = floor(Fl::event_x()*(MAX_CHANNEL + 2*XBORDER)/w());
-		FLHistcursorY[0] = floor((h()-Fl::event_y())*(YSTRIPS + 2*YBORDER)/h());
+		FLHistcursorX[0] = floor(Fl::event_x()*(MAX_CHANNEL)/w());
+		FLHistcursorY[0] = floor((h()-Fl::event_y())*(YSTRIPS)/h());
 
-		mouseHistPixel = FLHistcursorX[0] - XBORDER;
+		mouseHistPixel = FLHistcursorX[0];
 	}
 	
 	// now update the value displayed
